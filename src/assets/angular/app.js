5cd528661f7ad516ef5b10f4786b946a4a4cc75c
Nelly González <nelly.gonzalez@vincoorbis.com>
2018-08-14 15:55:14 -0500

    149160: Init angular application

---
+++ src/assets/angular/app.js
@@ -0,0 +1,7 @@
+'use strict';
+var app = angular.module('luke', ['ui.router','ngResource','ui.materialize']);
+
+app.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+}]);
